7cbaca86ef4f42b350a846fac411030bd44f7fb8
warbler <33575360+warbler@users.noreply.github.com>
2020-10-12 09:32:06 +0200

    add intro

--- app/components/Intro.vue
+++ app/components/Intro.vue
@@ -1,53 +1,73 @@
 <template>
-    <Page actionBarHidden="true" style="margin: 5px;">
-        <GridLayout rows="*, auto">
-          <Carousel row="0" height="100%" width="100%" indicatorColor="#757575" indicatorColorUnselected="#989898" finite="true" bounce="false" showIndicator="true" android:indicatorAnimation="swap">
-            <CarouselItem verticalAlignment="middle">
-                <GridLayout>
-                  <StackLayout horizontalAlignment="center" verticalAlignment="center">
-                    <Label text="Välkommen!" fontWeight="bold"/>
-                    <Label textWrap="true" text="Denna app använder du för att få information om tillgänglig pant inom ditt upphämtningsområde."/>
-                    <Label textWrap="true" text="Lorem ipsum."/>
-                  </StackLayout>
-                </GridLayout>
+  <Page actionBarHidden="true" class="coverImage" margin="0">
+    <GridLayout width="100%" height="90%">
+      <Image src="~/assets/images/Pantr_logo@3x.png" stretch="none" horizontalAlignment="center" />
+      <GridLayout height="500" background="white" margin="28" padding="0" borderRadius="20" androidElevation="12">
+        <GridLayout margin="30 30 15">
+          <Carousel width="100%" height="85%" indicatorColor="#26001c" indicatorColorUnselected="#f2f2f2" finite="true"
+                    bounce="false" showIndicator="true" android:indicatorAnimation="swap" marginBottom="45" verticalAlignment="bottom">
+            <CarouselItem>
+              <GridLayout>
+                <StackLayout>
+                  <Image src="~/assets/images/icon_can@3x.png" stretch="none" horizontalAlignment="center" />
+                  <Label text="Välkommen!" fontWeight="bold" fontSize="45" class="titleColor" />
+                  <Label class="bodyTextColor" textWrap="true" marginTop="17" lineHeight="3" fontSize="18"
+                         text="Denna app använder du för att få information om tillgänglig pant inom ditt upphämtningsområde."/>
+                </StackLayout>
+              </GridLayout>
             </CarouselItem>
-            <CarouselItem verticalAlignment="middle">
-                <GridLayout>
-                  <StackLayout horizontalAlignment="center" verticalAlignment="center">
-                    <Label text="Såhär fungerar det:" fontWeight="bold"/>
-                    <Label textWrap="true" text="Lorem ipsum."/>
-                  </StackLayout>
-                </GridLayout>
+            <CarouselItem>
+              <GridLayout>
+                <StackLayout>
+                  <Image src="~/assets/images/icon_mapmark@3x.png" stretch="none" horizontalAlignment="center" />
+                  <Label text="Såhär fungerar det:" fontWeight="bold" fontSize="25" class="titleColor" marginTop="41" />
+                  <Label class="bodyTextColor" textWrap="true" marginTop="22" lineHeight="3" fontSize="18"
+                         text="Lorem ipsum."/>
+                </StackLayout>
+              </GridLayout>
             </CarouselItem>
-            <CarouselItem verticalAlignment="middle">
-                <GridLayout>
-                  <StackLayout horizontalAlignment="center" verticalAlignment="center">
-                    <Label text="Frågor och support" fontWeight="bold"/>
-                    <Label textWrap="true" text="Du kan alltid kontakta oss på x@y.se om du stöter på problem, undrar över något eller vill lämna feedback."/>
-                  </StackLayout>
-                </GridLayout>
+            <CarouselItem>
+              <GridLayout>
+                <StackLayout>
+                  <Image src="~/assets/images/icon_que@3x.png" stretch="none" horizontalAlignment="center" />
+                  <Label text="Frågor & support" fontWeight="bold" fontSize="25" class="titleColor" marginTop="41" />
+                  <Label class="bodyTextColor" textWrap="true" marginTop="22" lineHeight="3" fontSize="18">
+                    <FormattedString>
+                      <Span text="Stöter du på något problem eller undrar om något gällande appen så kan du alltid kontakta oss på"/>
+                      <Span text=" info@pantr-app.se " fontWeight="bold" />
+                      <Span text="Vi försöker svara så snabbt vi kan."/>
+                    </FormattedString>
+                  </Label>
+                </StackLayout>
+              </GridLayout>
             </CarouselItem>
           </Carousel>
-          <Button @onTap="onReadyTap" row="1" text="Jag är redo" verticalAlignment="bottom"/>
         </GridLayout>
-    </Page>
+      </GridLayout>
+      <Button @onTap="onReadyTap" text="Jag är redo" verticalAlignment="bottom" textTransform="none" background="#1f2d40" color="white" borderRadius="40" width="60%" height="57" fontSize="21"/>
+    </GridLayout>
+  </Page>
 </template>
 
 <script>
-  import PlaceMarker from './PlaceMarker'
-
-  export default {
-    data() {
-      return {
-      }
-    },
-    methods: {
-      onReadyTap() {
-        this.$navigateTo(PlaceMarker);
-      }
+import App from './App'
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    onReadyTap() {
+      this.$navigateTo(App);
     }
   }
+}
 </script>
 
 <style scoped>
+.coverImage {
+  background-image: url('~/assets/images/pantr_welcome_bg.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
 </style>
